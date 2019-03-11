2b13550b41c61de1c56e4364b64ceee658f28e10
g770728y <g770728y@163.com>
2019-03-11 08:32:35 +0800

    删除Reach Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import { AppContext, AppStore } from './store';
-import { Router } from '@reach/router';
 
 class App extends Component {
   // TODO:
   render() {
     return (
       <AppContext.Provider value={new AppStore()}>
-        <div className="App">
-          <Router>
-            <Home path="/" />
-            <Desks path="/desks" />
-          </Router>
-        </div>
+        <div className="App" />
       </AppContext.Provider>
     );
   }
